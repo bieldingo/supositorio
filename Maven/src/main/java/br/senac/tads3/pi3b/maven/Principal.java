eb7b1bd7dc6b86cd35714997f8255053be1f9e3b
gabriel.sleal1 <gabriel.sleal1@CAS511749W0721.senacsp.edu.br>
2017-08-15 21:25:37 -0300

    Mandando um salve pro mundo

---
+++ Maven/src/main/java/br/senac/tads3/pi3b/maven/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads3.pi3b.maven;
+
+/**
+ *
+ * @author gabriel.sleal1
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Koé mundão !");
+    }
+
+}
